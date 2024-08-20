71fc0a97a99591f28760e2d8cc4ecd73bb87dd33
Ryan <ryanjerickson@gmail.com>
2024-08-19 20:32:52 -0500

    cards and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import cardFront from './images/CardFront.jpg'
+import cardBack from './images/CardBack.jpg'
 import './App.css'
 
 function App() {
@@ -5,7 +7,12 @@ function App() {
   return (
     <>
       <div>
-        Future Home of Greenwell's Taxidermy
+        <h1>Future Home of Greenwell's Taxidermy</h1>
+        <p>Contact us for picing and quotes at:</p>
+        <div className='cards'>
+        <img src={cardFront} /> <br />
+        <img src={cardBack} />
+        </div>
       </div>
     </>
   )
